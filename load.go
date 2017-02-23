716126992a7a0fb46fb89358811371fd1eba75dd
Daniel Fireman <danielfireman@gmail.com>
2017-02-23 16:03:26 -0300

    Loads of fixing trying to simplify the model and increase performance of the client

--- load.go
+++ load.go
@@ -10,76 +10,92 @@ import (
 )
 
 const (
-	loadDefSep     = ":"
-	constLoadDef   = "const"
+	loadDefSep = ":"
+	constLoadDef = "const"
 	poissonLoadDef = "poisson"
 )
 
 type LoadGen interface {
 	GetTicker() <-chan struct{}
+	Start() LoadGen
 }
 
 type ConstLoadGen struct {
-	Interval time.Duration
+	Qps float64
+	c   chan struct{}
 }
 
 func (g *ConstLoadGen) GetTicker() <-chan struct{} {
-	c := make(chan struct{})
+	if g.c == nil {
+		g.c = make(chan struct{})
+	}
+	return g.c
+}
+
+func (g *ConstLoadGen) Start() LoadGen {
 	go func() {
+		t := time.Duration((float64(1) / g.Qps) * 1e9)
 		for {
-			c <- struct{}{}
-			time.Sleep(g.Interval)
+			// Non-blocking ticking on c.
+			select {
+			case g.c <- struct{}{}:
+			default:
+			}
+			time.Sleep(t)
 		}
 	}()
-	return c
+	return g
 }
 
 func NewConstLoadGen(params []string) (LoadGen, error) {
-	qps, err := strconv.Atoi(params[0])
+	qps, err := strconv.ParseFloat(params[0], 64)
 	if err != nil {
 		return nil, err
 	}
-	req := qps / *workers // Number of requests per worker per second.
-	if req == 0 {
-		return nil, fmt.Errorf("To few requests per worker, please increase the qps or decrease the number of workers")
-	}
-	return &ConstLoadGen{time.Duration(time.Second.Nanoseconds() / int64(req))}, nil
+	return &ConstLoadGen{Qps:qps}, nil
 }
 
 type PoissonLoadGen struct {
 	// The rate parameter λ is a measure of frequency: the average rate of events (in this case, messages sent)
 	// per unit of time (in this case, seconds).
 	Lambda float64
+	c      chan struct{}
 }
 
 func (g *PoissonLoadGen) GetTicker() <-chan struct{} {
-	c := make(chan struct{})
+	if g.c == nil {
+		g.c = make(chan struct{})
+	}
+	return g.c
+}
+
+func (g *PoissonLoadGen) Start() LoadGen {
 	go func() {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		for {
+			// Non-blocking ticking on c.
 			// NOTE: Implementation follows:
 			// http://preshing.com/20111007/how-to-generate-random-timings-for-a-poisson-process/
-			c <- struct{}{}
-			time.Sleep(time.Duration(-math.Log(1.0-r.Float64())/g.Lambda) * time.Second)
+			select{
+				case g.c <- struct{}{}:
+				default:
+			}
+
+			time.Sleep(time.Duration((-math.Log(1.0 - r.Float64()) / float64(g.Lambda)) * 1e9))
 		}
 	}()
-	return c
+	return g
 }
 
-func NewPoissonLoadTicker(params []string) (LoadGen, error) {
-	qps, err := strconv.Atoi(params[0])
+func NewPoissonLoadGen(params []string) (LoadGen, error) {
+	qps, err := strconv.ParseFloat(params[0], 64)
 	if err != nil {
 		return nil, err
 	}
-	// TODO(danielfireman): Make workers a parameter.
-	numReq := (qps * int((*duration).Seconds())) / *workers
-	if numReq == 0 {
-		return nil, fmt.Errorf("To few requests per worker, please increase the qps or decrease the number of workers")
-	}
-	return &PoissonLoadGen{float64(numReq) / (*duration).Seconds()}, nil
+	return &PoissonLoadGen{Lambda:qps}, nil
 }
 
-func ParseLoadDef(def string) (LoadGen, error) {
+func NewLoadGen(def string) (LoadGen, error) {
 	p := strings.Split(def, loadDefSep)
 	if len(p) < 1 {
 		return nil, fmt.Errorf("Invalid load definition:%s", def)
@@ -88,7 +104,7 @@ func ParseLoadDef(def string) (LoadGen, error) {
 	case constLoadDef:
 		return NewConstLoadGen(p[1:])
 	case poissonLoadDef:
-		return NewPoissonLoadTicker(p[1:])
+		return NewPoissonLoadGen(p[1:])
 	default:
 		return nil, fmt.Errorf("Invalid load type:%s", p[0])
 	}
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"net"
 	"net/http"
 	"os"
@@ -19,30 +18,30 @@ import (
 )
 
 var (
-	workers     = flag.Int("c", 50, "Number of concurrent workers.")
-	addr        = flag.String("addr", "http://localhost:9200", "Elastic search HTTP address")
-	index       = flag.String("index", "wikipediax", "Index to perform queries")
+	addr = flag.String("addr", "http://localhost:9200", "Elastic search HTTP address")
+	index = flag.String("index", "wikipediax", "Index to perform queries")
 	resultsPath = flag.String("results_path", "~/results", "")
-	expID       = flag.String("exp_id", "1", "")
-	duration    = flag.Duration("duration", 30*time.Second, "Time sending load to the server.")
-	cint        = flag.Duration("cint", 5*time.Second, "Interval between metrics collection.")
-	load        = flag.String("load", "const:10", "Describes the load impressed on the server")
-	dict        = flag.String("dict", "small_dict.txt", "Dictionary of terms to use. One term per line.")
+	expID = flag.String("exp_id", "1", "")
+	duration = flag.Duration("duration", 30 * time.Second, "Time sending load to the server.")
+	cint = flag.Duration("cint", 5 * time.Second, "Interval between metrics collection.")
+	load = flag.String("load", "const:10", "Describes the load impressed on the server")
+	dict = flag.String("dict", "small_dict.txt", "Dictionary of terms to use. One term per line.")
+	timeout = flag.String("timeout", 5 * time.Second, "Timeout to be used in connections to ES.")
 )
 
 var (
 	succs, errs, reqs, shed uint64
-	logger                  = log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
-	respTimeStats           = &ResponseTimeStats{}
+	logger = log.New(os.Stdout, "", log.LstdFlags | log.Lshortfile)
+	respTimeStats = &ResponseTimeStats{}
 )
 
 func main() {
 	flag.Parse()
-	gen, err := ParseLoadDef(*load)
+	gen, err := NewLoadGen(*load)
 	if err != nil {
 		logger.Fatalf(err.Error())
 	}
-	logger.Printf("Starting sending load: #Workers:%d LoadDef:%s Duration:%v\n", *workers, *load, *duration)
+	logger.Printf("Starting sending load: LoadDef:%s Duration:%v\n", *load, *duration)
 
 	client, err := elastic.NewClient(
 		elastic.SetErrorLog(logger),
@@ -51,12 +50,13 @@ func main() {
 		elastic.SetSniff(false),
 		elastic.SetHealthcheck(false),
 		elastic.SetMaxRetries(0),
+		elastic.SetHealthcheckTimeout(*timeout),
 		elastic.SetHttpClient(&http.Client{
 			Timeout: 1 * time.Second,
 			Transport: &http.Transport{
 				Dial: (&net.Dialer{
-					Timeout:   1 * time.Second,
-					KeepAlive: 1 * time.Second,
+					Timeout:   *timeout,
+					KeepAlive: *timeout,
 				}).Dial,
 			},
 		}))
@@ -85,26 +85,38 @@ func main() {
 	go statsCollector(client, endStatsChan, &statsWaiter)
 	logger.Println("Stats collector started.")
 
-	endLoadChan := make(chan struct{}, *workers)
-	pauseLoadChan := make(chan float64, *workers)
-	var loadWaiter sync.WaitGroup
-	for i := 0; i < *workers; i++ {
-		loadWaiter.Add(1)
-		go worker(client, pauseLoadChan, endLoadChan, &loadWaiter, gen, terms)
-	}
-	logger.Printf("%d load workers have started. Waiting for their hard work...\n", *workers)
-
+	endLoadChan := make(chan struct{})
+	pauseLoadChan := make(chan float64)
+	nTerms := len(terms)
+	fire := gen.GetTicker()
+	go func() {
+		// Note: Having a single worker or a single load generator is a way to guarantee the load will obey to a
+		// certain  distribution. For instance, 10 workers generating load following a Poisson distribution is
+		// different  from having Poisson ruling the overall load impressed on the service.
+		for i := 0; ; i++ {
+			select {
+			case <-fire:
+				go sendRequest(client, pauseLoadChan, terms[i % nTerms])
+			case pt := <-pauseLoadChan:
+				time.Sleep(time.Duration(pt * 1e9) * time.Nanosecond)
+				fmt.Printf("Sleeping: %v\n", time.Duration(pt * 1000000000) * time.Nanosecond)
+				for range pauseLoadChan {
+				} // Emptying pause channel before continue.
+			case <-endLoadChan:
+				return
+			}
+		}
+	}()
+	logger.Printf("Worker has started. Waiting for their hard work...\n")
 	start := time.Now()
+	gen.Start()  // Start firing load.
 	<-time.Tick(*duration)
-	for i := 0; i < *workers; i++ {
-		endLoadChan <- struct{}{}
-	}
-	loadWaiter.Wait()
+	endLoadChan <- struct{}{}
 	close(endLoadChan)
 	close(pauseLoadChan)
-
 	dur := time.Now().Sub(start).Seconds()
-	logger.Printf("Done. QPS:%.2f #REQS:%d #SUCC:%d #ERR:%d #SHED:%d TP:%.2f", float64(reqs)/dur, reqs, succs, errs, shed, float64(succs)/dur)
+
+	logger.Printf("Done. AVGQPS:%.2f #REQS:%d #SUCC:%d #ERR:%d #SHED:%d AVGTP:%.2f", float64(reqs) / dur, reqs, succs, errs, shed, float64(succs) / dur)
 	atomic.StoreUint64(&reqs, 0)
 	atomic.StoreUint64(&succs, 0)
 	atomic.StoreUint64(&errs, 0)
@@ -118,36 +130,10 @@ func main() {
 	logger.Println("Load test finished successfully.")
 }
 
-func worker(client *elastic.Client, pause chan float64, end chan struct{}, wg *sync.WaitGroup, gen LoadGen, terms []string) {
-	defer wg.Done()
-	ctx := context.Background()
-	fire := gen.GetTicker()
-	// Avoiding default random synchronization block. Each worker can have its own random source.
-	// http://blog.sgmansfield.com/2016/01/the-hidden-dangers-of-default-rand/
-	nTerms := len(terms)
-	// NOTE: Tried to generate a random number per request and that damages performance in high load tests.
-	// Modulo operation is a way cheaper than the random number generator, even if we get rid of the synchronization
-	// lock from the default random generator.
-	start := rand.New(rand.NewSource(time.Now().Unix())).Intn(nTerms)
-	for {
-		select {
-		case <-fire:
-			sendRequest(ctx, client, pause, terms[start%nTerms])
-		case pt := <-pause:
-			time.Sleep(time.Duration(pt*1000000000) * time.Nanosecond)
-			fmt.Printf("Sleeping: %v\n", time.Duration(pt*1000000000)*time.Nanosecond)
-			for range pause {
-			} // Emptying pause channel before continue.
-		case <-end:
-			return
-		}
-	}
-}
-
-func sendRequest(ctx context.Context, client *elastic.Client, pauseChan chan float64, term string) {
+func sendRequest(client *elastic.Client, pauseChan chan float64, term string) {
 	atomic.AddUint64(&reqs, 1)
 	q := elastic.NewTermQuery("text", term)
-	resp, err := client.Search().Index(*index).Query(q).Do(ctx)
+	resp, err := client.Search().Index(*index).Query(q).Do(context.Background())
 	if err != nil {
 		atomic.AddUint64(&errs, 1)
 		return
