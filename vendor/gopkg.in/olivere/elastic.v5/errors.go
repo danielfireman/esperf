c3c37493c7e271707d492fa82ef850d9f7dec1fb
Daniel Fireman <danielfireman@gmail.com>
2017-03-02 01:26:56 -0300

    Changing elastic library to return headers in case of errors

--- vendor/gopkg.in/olivere/elastic.v5/errors.go
+++ vendor/gopkg.in/olivere/elastic.v5/errors.go
@@ -38,17 +38,23 @@ func checkResponse(req *http.Request, res *http.Response, ignoreErrors ...int) e
 // createResponseError creates an Error structure from the HTTP response,
 // its status code and the error information sent by Elasticsearch.
 func createResponseError(res *http.Response) error {
+	headerCopy := make(http.Header)
+	for k, vv := range res.Header {
+		for _, v := range vv {
+			headerCopy.Add(k, v)
+		}
+	}
 	if res.Body == nil {
-		return &Error{Status: res.StatusCode}
+		return &Error{Status: res.StatusCode, Header: headerCopy}
 	}
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return &Error{Status: res.StatusCode}
+		return &Error{Status: res.StatusCode, Header: headerCopy}
 	}
 	errReply := new(Error)
 	err = json.Unmarshal(data, errReply)
 	if err != nil {
-		return &Error{Status: res.StatusCode}
+		return &Error{Status: res.StatusCode, Header: headerCopy}
 	}
 	if errReply != nil {
 		if errReply.Status == 0 {
@@ -56,13 +62,14 @@ func createResponseError(res *http.Response) error {
 		}
 		return errReply
 	}
-	return &Error{Status: res.StatusCode}
+	return &Error{Status: res.StatusCode, Header: headerCopy}
 }
 
 // Error encapsulates error details as returned from Elasticsearch.
 type Error struct {
 	Status  int           `json:"status"`
 	Details *ErrorDetails `json:"error,omitempty"`
+	Header  http.Header   `json:"header"`
 }
 
 // ErrorDetails encapsulate error details from Elasticsearch.
