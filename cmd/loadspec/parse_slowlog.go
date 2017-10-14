3307817e0deae916cbdcbca96959969253e8220a
Daniel Fireman <danielfireman@gmail.com>
2017-10-14 09:57:33 -0300

    Refactor log entry

--- cmd/loadspec/parse_slowlog.go
+++ cmd/loadspec/parse_slowlog.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"regexp"
 	"sort"
 	"strings"
 	"time"
@@ -22,50 +21,6 @@ var (
 func init() {
 	parseSlowlogCmd.Flags().StringSliceVar(&indexOverride, "index_override", []string{}, "Override slowlog indexes. It is a list, flag could be repeated if you would the loadtest to hit many indexes.")
 	parseSlowlogCmd.Flags().DurationVar(&maxDuration, "max_duration", time.Duration(0), "Maximum duration of the generated loadspec. It could be smaller, if the slowlog comprise a smaller time frame.")
-}
-
-// These constants need to be in sync with the regular expression bellow.
-const (
-	logTypeField    = "log_type"
-	hostField       = "host"
-	timestampField  = "ts"
-	indexField      = "index"
-	typesField      = "types"
-	searchTypeField = "search_type"
-	sourceField     = "source"
-	numFields       = 7
-)
-
-var matcherRE = regexp.MustCompile(`\[(?P<ts>[^]]+)\].?\[.*\].?\[(?P<log_type>[^]]+)\].?\[(?P<host>[^]]+)\].?\[(?P<index>[^]]+)\].?\[.*\].*types\[(?P<types>[^]]+)\].*search_type\[(?P<search_type>[^]]+)\].*source\[(?P<source>.*)\], extra_source`)
-var subExpNames = matcherRE.SubexpNames()
-
-type slowlogEntry struct {
-	LogType    string
-	Host       string
-	Timestamp  string
-	Index      string
-	Types      string
-	SearchType string
-	Source     string
-}
-
-func decodeSlowlogEntry(row string) slowlogEntry {
-	fields := make(map[string]string, numFields)
-	matches := matcherRE.FindAllStringSubmatch(row, -1)[0]
-	for i, m := range matches {
-		if i > 0 { // Removing the first match, which is the whole line.
-			fields[subExpNames[i]] = m
-		}
-	}
-	return slowlogEntry{
-		LogType:    fields[logTypeField],
-		Host:       fields[hostField],
-		Timestamp:  fields[timestampField],
-		Index:      fields[indexField],
-		Types:      fields[typesField],
-		SearchType: fields[searchTypeField],
-		Source:     fields[sourceField],
-	}
 }
 
 var parseSlowlogCmd = &cobra.Command{
